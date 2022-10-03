4f1440fa2775c92e89c28b235bd16f9c0befceda
Aman <amanyadav6750@gmail.com>
2022-10-03 15:49:00 +0530

    update smooth user flow

--- TenderManagementSystem/src/com/TMS/Main/Main.java
+++ TenderManagementSystem/src/com/TMS/Main/Main.java
@@ -25,11 +25,11 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		  System.out.println("please enter your Username");
+		  System.out.println("*********please enter your Admin Username************\n");
           
           String uname = sc.next();
           
-          System.out.println("please enter your Password");
+          System.out.println(" \n*************** please enter your Admin Password***********");
           
           String upass = sc.next();
           
@@ -46,44 +46,48 @@ public class Main {
        		   
        		   if(pass.equals(upass)) {
        			   
-       			   System.out.println("Welcome as an Admin...!");
+       			   System.out.println("\n Welcome as an Admin...!");
        			   
        			   while(true) {
        			   
-       			   System.out.println("Select any one of the following: ");
+       			   System.out.println(" \nSelect any one of the following: ");
        			   
-       			   System.out.println("2. Register new Vendor."+"\n"+ "3. View all the vendors."+"\n"+"4. Create new tenders."+"\n"+ "5. View All the Tenders."+"\n"+"6. View All the Bids of a tender."+"\n"+"7. Assign tender to a vendor.");
+       			   System.out.println("Select (2) to Register new Vendor."+"\n"+ "Select (3) to  View all the vendors."+"\n"+"Select (4) to  Create new tenders."+"\n"+ "Select (5) to View All the Tenders."+"\n"+"Select (6) to  View All the Bids of a tender."+"\n"+"Select (7) to  Assign tender to a vendor.");
        			
-       			   System.out.println("8. return back to admin menu");
+       			   System.out.println("Select (8) to  To remove tendor from list");
+       			   System.out.println("Select (9) to  To update tendor ");
 
-       			   System.out.println("9. return back to main menu");
+       			   
+       			   System.out.println("Select (10) to  return back to admin menu");
+
+       			   System.out.println("Select (11) to  return back to main menu");
        					   
        					   int k = sc.nextInt();
        			   
        			   switch (k) {
 					case 2: {
 						
-						System.out.println("WELCOME TO REGISTER VENDOR");
+						System.out.println("\n WELCOME TO REGISTER VENDOR");
 						
-						System.out.println("Enter Vendor id");
+						System.out.println("\n Enter Vendor id");
 						String vid = sc.next();
 						
-						System.out.println("Enter Vendor name");
+						System.out.println("\n Enter Vendor name");
 						String name = sc.next();
 						
-						System.out.println("Enter Vendor mobile Number");
+						System.out.println("\n Enter Vendor mobile Number");
 						String mobile = sc.next();
 						
-						System.out.println("Enter Vendor email");
+						System.out.println("\n Enter Vendor email");
 						String email = sc.next();
 						
-						System.out.println("Enter Vendor password for register");
+						System.out.println(" \n Enter Vendor password for register");
 						String password = sc.next();
 						
-						System.out.println("Enter Vendor Company name");
+						System.out.println(" \n Enter Vendor Company name");
 						String company = sc.next();
 						
-						System.out.println("Enter Vendor address");
+						System.out.println("\n Enter Vendor address");
 						String address = sc.next();
 						
 						
@@ -102,7 +106,7 @@ public class Main {
 					}
 					case 3:{
 						
-						System.out.println("LIST OF VENDORS:");
+						System.out.println("\n LIST OF VENDORS:");
 						
 						vendorDao dao = new VendorDaoImpl();
 						List<Vendorbean> list=dao.viewAllVendors();
@@ -116,6 +120,119 @@ public class Main {
 						break;
 					}
 					case 4:{
+						
+						System.out.println("\n Enter tendor id");
+						String tid = sc.next();
+						
+						System.out.println("\n Enter tendor name");
+						String tname = sc.next();
+						
+						System.out.println("\n Enter tendor type");
+						String ttype = sc.next();
+						
+						System.out.println("\n Enter tendor price");
+						int tprice = sc.nextInt();
+						
+						System.out.println("\n enter description of the tendor");
+						String tdesc = sc.next();
+						
+						System.out.println("\n Enter tendor deadline date");
+						String tdeadline = sc.next();
+						
+						System.out.println("\n Enter tendor location");
+						String tlocation = sc.next();
+						
+						
+						tenderbean vb = new tenderbean( tid, tname, ttype, tprice, tdesc, tdeadline,tlocation);
+						
+						TendorDao dao = new TendorDaoImpl();
+						
+						String status =dao.CreatenewTendor(vb);
+						
+						System.out.println(status);
+						
+//						admin();
+						break;
+					}
+					case 5:{
+						System.out.println("\n LIST OF TENDORS:");
+						
+						
+						TendorDao dao = new TendorDaoImpl();
+						List<tenderbean> list =dao.getAllTenders();
+						
+						for(tenderbean t: list) {
+							System.out.println(t);
+						}
+						
+//						admin();
+						break;
+					}
+					case 6:{
+						
+						System.out.println("\n Enter Tendor id :");
+						
+						String tid = sc.next();
+						
+						BidderDao dao = new BidderDaoImpl();
+						
+						List<Bidderbean> list=dao.getAllBidsOnTendor(tid);
+						
+						if(list.size()==0) {
+							System.out.println("Currntly no bid is there on tendor.");
+						}else {
+							
+							for(Bidderbean t: list) {
+								System.out.println(t);
+							}
+						}
+						
+//						admin();
+						break;
+					}
+					
+					case 7:{
+						
+						System.out.println("\n  TO ASSIGN TENDOR TO VENDOR WE REQUIRE:");
+						
+						System.out.println(" ENTER VENDOR ID");
+						String vid = sc.next();
+						
+						System.out.println("ENTER TENDOR ID");
+						String tid = sc.next();
+						
+						System.out.println("ENTER BIDDER ID");
+						String bid = sc.next();
+						
+						TendorDao dao = new TendorDaoImpl();
+						String message =dao.assignTender(tid, vid, bid);
+						
+						System.out.println(message);
+						
+//						admin();
+						break;
+					}
+					case 8:{
+						
+						System.out.println("\n Enter Tendor id : ");
+						
+						String tid = sc.next();
+						
+						TendorDao dao = new TendorDaoImpl();
+						if(dao.removeTendor(tid)) {
+							
+							System.out.println("Tendor removed Sucessfully");
+						}else {
+							System.out.println("Tendor removed Failed");
+
+						}
+						
+						
+						
+						break;
+					}
+					
+					case 9:{
 						
 						System.out.println("Enter tendor id");
 						String tid = sc.next();
@@ -143,79 +260,20 @@ public class Main {
 						
 						TendorDao dao = new TendorDaoImpl();
 						
-						String status =dao.CreatenewTendor(vb);
-						
-						System.out.println(status);
-						
-//						admin();
-						break;
-					}
-					case 5:{
-						System.out.println("LIST OF TENDORS:");
-						
-						
-						TendorDao dao = new TendorDaoImpl();
-						List<tenderbean> list =dao.getAllTenders();
-						
-						for(tenderbean t: list) {
-							System.out.println(t);
-						}
-						
-//						admin();
-						break;
-					}
-					case 6:{
-						
-						System.out.println("Enter Tendor id :");
-						
-						String tid = sc.next();
-						
-						BidderDao dao = new BidderDaoImpl();
-						
-						List<Bidderbean> list=dao.getAllBidsOnTendor(tid);
-						
-						if(list.size()==0) {
-							System.out.println("Currntly no bid is there on tendor.");
-						}else {
-							
-							for(Bidderbean t: list) {
-								System.out.println(t);
-							}
-						}
-						
-//						admin();
-						break;
-					}
-					
-					case 7:{
-						
-						System.out.println(" TO ASSIGN TENDOR TO VENDOR WE REQUIRE:");
-						
-						System.out.println(" ENTER VENDOR ID");
-						String vid = sc.next();
-						
-						System.out.println("ENTER TENDOR ID");
-						String tid = sc.next();
-						
-						System.out.println("ENTER BIDDER ID");
-						String bid = sc.next();
-						
-						TendorDao dao = new TendorDaoImpl();
-						String message =dao.assignTender(tid, vid, bid);
+						String message =dao.updateTender(vb);
 						
 						System.out.println(message);
 						
-//						admin();
 						break;
 					}
 					
-					case 8:{
+					case 10:{
 						
 						admin();
 						break ;
 						
 					}
-					case 9:{
+					case 11:{
 						
 						main(null);
 						break ;
@@ -253,11 +311,11 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		  
-        System.out.println("please enter your Username");
+        System.out.println("\n please enter your Username");
         
         String uname = sc.next();
         
-        System.out.println("please enter your Password");
+        System.out.println("\n please enter your Password");
         
         String upass = sc.next();
         
@@ -276,18 +334,18 @@ public class Main {
      		   
      		   if((pass.equals(upass)) && (name.equals(uname))) {
      			   
-     			   System.out.println("Welcome as an Vendor...!");
+     			   System.out.println("\n Welcome as an Vendor...!");
      			   
      			   
      			   while(true) {
      			   
      			   		System.out.println("Select any one of the following: ");
      			   
-     			   System.out.println("2. View all the current Tenders"+"\n"+ "3.Place a Bid against a Tender."+"\n"+"4. View status of a Bid(Whether Selected or Not)."+"\n"+ "5. View his own Bid History");
+     			   System.out.println("Select (2) to  View all the current Tenders"+"\n"+ "Select (3) to Place a Bid against a Tender."+"\n"+"Select (4) to  View status of a Bid(Whether Selected or Not)."+"\n"+ "Select (5) to  View his own Bid History");
      			   
-     			   System.out.println("6. return to back vendor menu");
+     			   System.out.println("Select (6) to  return to back vendor menu");
 
-       			   System.out.println("7. return to back main menu");
+       			   System.out.println("Select (7) to  return to back main menu");
 
      					   
      					   int k = sc.nextInt();
@@ -295,7 +353,7 @@ public class Main {
      					   switch (k) {
 							case 2: {
 								
-								System.out.println("VIEWING ALL THE CURRENT TENDORS");
+								System.out.println(" \n VIEWING ALL THE CURRENT TENDORS");
 								
 								
 								
@@ -315,7 +373,7 @@ public class Main {
 							case 3: {
 								
 								
-								System.out.println("PLACING BID AGAINST TENDOR");
+								System.out.println("\n PLACING BID AGAINST TENDOR");
 								
 								System.out.println("ENTER BIDDER ID");
 								String bid = sc.next();
@@ -346,7 +404,7 @@ public class Main {
 							
 							case 4: {
 								
-								System.out.println("WELCOME TO View status of a Bid(Whether Selected or Not). ");
+								System.out.println("\n WELCOME TO View status of a Bid(Whether Selected or Not). ");
 								
 								
 																	
@@ -359,13 +417,13 @@ public class Main {
 								System.out.println("ENTER YOUR VENDOR ID:");
 								String vid = sc.next();
 								
-								System.out.println("============ACCEPTANCE DETAILS===========");
+								System.out.println("\n ============ACCEPTANCE DETAILS===========");
 								System.out.println();
 								BidderDao dao = new BidderDaoImpl();
 								String message = dao.acceptBid(bid, tid, vid);
 								System.out.println(message);
 								
-								System.out.println("=============REJECTION DETAILS=============");
+								System.out.println("\n =============REJECTION DETAILS=============");
 								
 								System.out.println();
 								
@@ -381,7 +439,7 @@ public class Main {
 							
 							case 5: {
 
-								System.out.println("View his own Bid History....");
+								System.out.println("\n View his own Bid History....");
 								
 								
 								System.out.println("ENTER YOUR VENDOR ID:");
@@ -447,10 +505,12 @@ public class Main {
 
 		Scanner sc = new Scanner (System.in);
 		
-           System.out.println("There are 2 users in the system:");
+		System.out.println("\n ((((((((((WELCOME TO TENDOR MANAGEMENT SYSTEM)))))))))))\n ");
+		
+           System.out.println("               There are 2 users in the system:");
            
-           System.out.println("select 1 for Administrator");
-           System.out.println("select 2 for Vendor");
+           System.out.println("\n****************** select 1 for Admin******************");
+           System.out.println("\n ******************select 2 for Vendor******************");
 		
 
            int n = sc.nextInt();
@@ -467,7 +527,7 @@ public class Main {
         	   
            }else {
         	   
-        	   System.out.println("rasta napoo");
+        	   System.out.println("!!!!!!!!!!!!!!!!!!rasta napoo!!!!!!!!!!!!!!!!!!");
         	   
            }
 
